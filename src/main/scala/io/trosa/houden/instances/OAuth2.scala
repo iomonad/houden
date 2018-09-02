875ff901773b721c3924d19cdef504bb4de8fc9f
Clement Trosa <me@trosa.io>
2018-09-02 16:06:35 +0200

    Added boilerplate structure

---
+++ src/main/scala/io/trosa/houden/instances/OAuth2.scala
@@ -0,0 +1,11 @@
+package io.trosa.houden.instances
+
+import cats._
+import cats.instances._
+import cats.implicits._
+
+trait OAuth2 {
+
+  implicit def AOuth2Monad(client: String) = ???
+
+}
---
+++ src/main/scala/io/trosa/houden/types/package.scala
@@ -0,0 +1,5 @@
+package io.trosa.houden
+
+package object types {
+
+}
